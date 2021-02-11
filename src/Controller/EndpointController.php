aa1c193fa96ab073307c10c1ca3d97fc8626635e
RafalMalik <rafalmalik.info@gmail.com>
2021-02-11 19:28:24 +0100

    Added endpoint controller.

---
+++ src/Controller/EndpointController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EndpointController extends AbstractController
+{
+    /**
+     * @Route("/endpoint", name="endpoint")
+     */
+    public function index(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/EndpointController.php',
+        ]);
+    }
+
+    /**
+     * @Route("/endpoint", name="endpoint")
+     */
+    public function create(LoggerInterface  $logger): Response
+    {
+        $logger->info('ELO');
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/EndpointController.php',
+        ]);
+    }
+}
